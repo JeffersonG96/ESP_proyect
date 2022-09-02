acaf330dd39f9bf3518c4f0c4caf6c6985c144ab
Jefferson Guaman <jefersonguaman94@gmail.com>
2022-09-01 23:53:25 -0500

    Conecta con token (sendId) - Falta sensores

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,24 @@
 //variables para server
 String email = "test1@test.com";
 String password = "123456";
-String webApi = "http://192.168.100.149:3000/api/login/";
-const char* mqttServer = "192.168.100.149";
+String webApi = "http://192.168.100.160:3000/api/login/";
+String webApiFind = "http://192.168.100.160:3000/api/login/find";
+const char* mqttServer = "192.168.100.160";
 int mqttPort = 1883;
 
 // //
-// String responseBody; 
+// String responseBody;
 
-//pines 
+//pines
 #define led 2
 
 //credenciales WiFi
 const char *wifi_ssid = "TELECOM_Pato";
 const char *wifi_pass = "P@to1984$";
 
-//funciones 
-void clear();
+//funciones
 void conectar();
+bool SendDataServer();
 bool getMqttCredentials();
 void checkMqttConnection();
 bool reconnect();
@@ -33,12 +34,13 @@ void procesarSensores();
 void sendData();
 
 
-//Variables globales 
+//Variables globales
 long lastReconnect;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 DynamicJsonDocument mqttDataDoc(1024);
+StaticJsonDocument<512> dataServer;
 
 
 void setup() {
@@ -46,11 +48,11 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(led, OUTPUT);
-  clear();
   conectar();
-  getMqttCredentials();
-  
-  
+  SendDataServer();
+  // getMqttCredentials();
+
+
 }
 
 void loop() {
@@ -72,16 +74,13 @@ void procesarSensores(){
 
   //obtener temp
   int temp = random(1,100);
-  JsonObject usuario = mqttDataDoc["usuario"];
-// const char* usuario_uid = usuario["uid"];
-  // mqttDataDoc["variables"];
   mqttDataDoc["variables"][0]["variableName"] = "temp";
   mqttDataDoc["variables"][0]["frecuencia"] = 10; //*frecuencia a enviar el dato
   mqttDataDoc["variables"][0]["last"]["value"] = temp;
-    int dif = temp - prev_temp;
+  int dif = temp - prev_temp;
   if (dif < 0) {dif *= -1;}
 
-  if (dif >= 40) {
+  if (dif >= 20) {
     mqttDataDoc["variables"][0]["last"]["save"] = 1;
   }else{
     mqttDataDoc["variables"][0]["last"]["save"] = 0;
@@ -90,8 +89,8 @@ void procesarSensores(){
 
   //obtener BPM
   int BPM = random(1,100);
-  mqttDataDoc["variables"][1]["variableName"] = "BPM";
-  mqttDataDoc["variables"][1]["frecuencia"] = 30;
+  mqttDataDoc["variables"][1]["variableName"] = "heart";
+  mqttDataDoc["variables"][1]["frecuencia"] = 10;
   mqttDataDoc["variables"][1]["last"]["value"] = BPM;
     dif = BPM - prev_hum;
   if (dif < 0) {dif *= -1;}
@@ -103,11 +102,10 @@ void procesarSensores(){
   }
   prev_hum = BPM;
 
-
-
+  //*spo2
     int RES = random(1,100);
-  mqttDataDoc["variables"][2]["variableName"] = "res";
-  mqttDataDoc["variables"][2]["frecuencia"] = 5;
+  mqttDataDoc["variables"][2]["variableName"] = "spo2";
+  mqttDataDoc["variables"][2]["frecuencia"] = 10;
   mqttDataDoc["variables"][2]["last"]["value"] = RES;
     dif = RES- prev_hum;
   if (dif < 0) {dif *= -1;}
@@ -118,6 +116,21 @@ void procesarSensores(){
     mqttDataDoc["variables"][2]["last"]["save"] = 0;
   }
   prev_hum = RES;
+
+  //*status
+    int status = random(1,100);
+  mqttDataDoc["variables"][3]["variableName"] = "status";
+  mqttDataDoc["variables"][3]["frecuencia"] = 10; //*frecuencia a enviar el dato
+  mqttDataDoc["variables"][3]["last"]["value"] = status;
+    dif = status - prev_temp;
+  if (dif < 0) {dif *= -1;}
+
+  if (dif >= 20) {
+    mqttDataDoc["variables"][3]["last"]["save"] = 0;
+  }else{
+    mqttDataDoc["variables"][3]["last"]["save"] = 0;
+  }
+  prev_temp = status;
 
 
 }
@@ -134,31 +147,23 @@ void sendData(){
   if((now - varsLastS[i]) > frec * 1000){
    varsLastS[i] = millis();
 
-  JsonObject usuario = mqttDataDoc["usuario"];
-  String usuario_uid = usuario["uid"];
+  String uid = mqttDataDoc["uid"];
   String variableName =mqttDataDoc["variables"][i]["variableName"];
-  String topic = usuario_uid +"/" + variableName + "/sdata";
-  Serial.println(topic);
-  
+  String topic = uid +"/" + variableName + "/sdata";
+  // Serial.println(topic);
+
   String toSend = "";
   serializeJson(mqttDataDoc["variables"][i]["last"], toSend);
 
   client.publish(topic.c_str(),toSend.c_str());
 
   }
-  }  
+  }
 
 }
 
 
 //TODO Funciones propias de la ESP y server
-void clear() {
-  Serial.println();
-  Serial.write(27);
-  Serial.print("[2J"); 
-  Serial.write(27);
-  Serial.print("[H");
-}
 //?COnectar wiFI
 void conectar(){
   WiFi.begin(wifi_ssid,wifi_pass);
@@ -178,13 +183,13 @@ void conectar(){
   Serial.print("IP => ");
   Serial.print(WiFi.localIP());
 }
-//?Conectar al servidor 
-bool getMqttCredentials(){
-  Serial.print(" \n ***Obteniendo Credenciales***");
+//?Conectar al servidor
+bool SendDataServer(){
+  Serial.print(" \n ***ENVIANDO CREDENCIALES AL SERVIDOR***");
   delay(1000);
   //preparo un objeto con las credenciales para enviar
   HTTPClient http ;
-  String responseBody; 
+  String responseBody;
 
   StaticJsonDocument<512> buff;
   String jsonParams;
@@ -199,24 +204,23 @@ bool getMqttCredentials(){
   http.begin(webApi);
   http.addHeader("Content-Type","application/json");
   int statusCode = http.POST(jsonParams);
-  delay(2000);
-  
+  delay(500);
+
   Serial.println(statusCode);
 
   if (statusCode != 200){
     Serial.println("##### ERROR EN LOS DATOS INGRESADOS #####");
-    http.end(); //finaliza la petición 
+    http.end(); //finaliza la petición
+    ESP.restart();
     return false;
   }
 
   if (statusCode == 200){
     responseBody = http.getString();
-    delay(2000);
-  
-  StaticJsonDocument<200> filter;
-  filter["usuario"] = true; 
+        delay(500);
 
-DeserializationError error = deserializeJson(mqttDataDoc, responseBody, DeserializationOption::Filter(filter));
+DeserializationError error = deserializeJson(dataServer, responseBody);
+// DeserializationError error = deserializeJson(mqttDataDoc, responseBody);
 
 if (error) {
   Serial.print("deserializeJson() failed: ");
@@ -224,8 +228,71 @@ if (error) {
   return false;
 }
 
-JsonObject usuario = mqttDataDoc["usuario"];
+JsonObject usuario = dataServer["usuario"];
 const char* usuario_uid = usuario["uid"]; // "62d9c65473a36709d8a9326f"
+
+
+serializeJsonPretty(dataServer, Serial);
+http.end();
+return true;
+  }
+
+
+}
+
+bool getMqttCredentials(){
+  Serial.print(" \n ###### ENVIANDO UID ######");
+  delay(1000);
+  //preparo un objeto con las credenciales para enviar
+  HTTPClient http ;
+  String responseBody;
+
+  StaticJsonDocument<512> buff;
+  String jsonParams;
+
+  JsonObject usuario = dataServer["usuario"];
+  String uid = usuario["uid"];
+  String token = dataServer["token"];
+  Serial.printf(token.c_str());
+
+  buff["uid"] = uid;
+  // buff["password"] = password;
+
+  serializeJson(buff, jsonParams);
+
+  http.begin(webApiFind);
+  http.addHeader("Content-Type","application/json");
+  http.addHeader("x-token",token);
+  int statusCode = http.POST(jsonParams);
+  delay(500);
+
+  Serial.println(statusCode);
+
+  if (statusCode != 200){
+    Serial.println("##### ERROR EN LOS DATOS INGRESADOS #####");
+    http.end(); //finaliza la petición
+    ESP.restart();
+    return false;
+  }
+
+  if (statusCode == 200){
+    responseBody = http.getString();
+    delay(2000);
+
+  // StaticJsonDocument<200> filter;
+  // filter["usuario"] = true;
+
+DeserializationError error = deserializeJson(mqttDataDoc, responseBody);
+// , DeserializationOption::Filter(filter)
+
+if (error) {
+  Serial.print("deserializeJson() failed: ");
+  Serial.println(error.c_str());
+  return false;
+}
+
+// JsonObject usuario = mqttDataDoc["usuario"];
+// const char* usuario_uid = usuario["uid"]; // "62d9c65473a36709d8a9326f"
 
 serializeJsonPretty(mqttDataDoc, Serial);
 http.end();
@@ -249,8 +316,8 @@ if(!client.connected()){
     if(reconnect()){
       lastReconnect = 0;
     }
-  } 
-  
+  }
+
 } else { client.loop(); }
 }
 
@@ -263,22 +330,24 @@ bool reconnect(){
   }
 
   client.setServer(mqttServer, mqttPort);
-  Serial.println("Intentando Conectar al Broker MQTT (EMQX)");  
-  
-  
-  const char* emqxUsername = "user";
-  const char* emqxPassword = "root";
-  JsonObject usuario = mqttDataDoc["usuario"];
-  String usuario_uid = usuario["uid"];
-  String topic = usuario_uid + "/+/sdata"; 
+  Serial.println("Intentando Conectar al Broker MQTT (EMQX)");
+
+  const char* userName = mqttDataDoc["username"];
+  const char* password = mqttDataDoc["password"];
 
 
-  if(client.connect(usuario_uid.c_str(), emqxUsername, emqxPassword)){
-   Serial.println(""); 
+  JsonObject usuario = dataServer["usuario"];
+  String uid = usuario["uid"];
+  String topic = uid + "/+/sdata";
+  String dId = "device_" + uid;
+
+
+  if(client.connect(dId.c_str(), userName, password)){
+   Serial.println("");
    Serial.println("CONECTADO AL BROKER MQTT");
   if(client.subscribe(topic.c_str())){
     Serial.println("Subscrito: " + topic);
-  } 
+  }
   return true;
 
   } else {
